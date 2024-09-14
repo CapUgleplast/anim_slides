526d44e1f70bb9cf550df5db1fb56016f308da66
CapUgleplast <kpowdm@yandex.ru>
2024-09-14 21:53:03 +0700

    upd

--- services/content.service.ts
+++ services/content.service.ts
@@ -11,27 +11,27 @@ export default class ContentService {
             const imgProp: CoverModel[] = [
                 {
                     id: 1,
-                    src: '/images/image1.png'
+                    src: '/images/image1.webp'
                 },
                 {
                     id: 2,
-                    src: '/images/image2.png'
+                    src: '/images/image2.webp'
                 },
                 {
                     id: 3,
-                    src:  '/images/image3.png',
+                    src:  '/images/image3.webp',
                 },
                 {
                     id: 4,
-                    src: '/images/image4.png',
+                    src: '/images/image4.webp',
                 },
                 {
                     id: 5,
-                    src: '/images/image5.png',
+                    src: '/images/image5.webp',
                 },
                 {
                     id: 6,
-                    src: '/images/image6.png',
+                    src: '/images/image6.webp',
                 },
                 {
                     id: 7,
@@ -47,7 +47,8 @@ export default class ContentService {
         return new Promise((resolve, reject) => {
             const promotionObj =  {
                 button: 'Акция',
-                title: 'Квартиры месяца со скидкой -30%'
+                title: 'Квартиры месяца со скидкой -30%',
+                link: '#'
             }
 
             resolve(promotionObj);
